2315e65705ab8b274524593e5a684725c871fc3c
TiUP <zhangl@tiup.us>
2019-12-13 13:09:39 +0800

    第一节课作业

--- server/index.js
+++ server/index.js
@@ -5,20 +5,24 @@ import App from '../src/app.js'
 
 const app = express()
 
+//  家在使用静态资源
+app.use(express.static('public'))
+
 app.get('/', (req, res) => {
-    const Page = <App></App>
+    // const Page = <App title="开本吧"></App>
     // 将react 组建 解析为 html 
-    const content = renderToString(Page)
+    const content = renderToString(App)
     res.end(`
         <html>
             <head>
-                <meta charset="utf-8 />
+                <meta charset="UTF-8">
                 <title>resct ssr</title>
             </head>
             <body>
                 <div id="root">
                     ${content}
                 </div>
+                <script src="/bundle.js"></script>
             </body>
         </html>
     `)
